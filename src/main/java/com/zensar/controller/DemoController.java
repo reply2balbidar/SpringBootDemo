13257009e508770de71b6fd3f2019e981174ce45
balbindar <reply2balbindar@hotmail.com>
2022-09-26 23:40:08 +0530

    Spring Demo example code commit

---
+++ src/main/java/com/zensar/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.zensar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+	
+	@GetMapping("/sayHello")
+	public String sayHello() {
+		
+		return "Welcome to demo example";
+	}
+}
